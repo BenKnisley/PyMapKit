17cbab36817642cfc998ebaa728bfff1716ad214
Ben Knisley <benknisley@gmail.com>
2020-03-18 17:44:16 -0400

    Fixed all issues with pylint, added setup.py, and MIT license.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Author: Ben Knisley [benknisley@gmail.com]
+Date: 17 March, 2020
+"""
+from setuptools import setup
+
+setup(
+    name = "MapEngine",
+    version = "0.0.0",
+    author = "Ben Knisley",
+    author_email = "benknisley@gmail.com",
+    description = ("A module for rendering maps."),
+    url = "https://github.com/BenKnisley/MapEngine",
+    license = "MIT",
+    keywords = "GIS map MapEngine",
+    package_dir={'': 'src'},
+    py_modules=["MapEngine", "VectorLayer", "CairoMapPainter"],
+    long_description="...",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Scientific/Engineering :: GIS",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
+
+
