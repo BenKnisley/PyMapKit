c7917fa6e9b4da0f460166d3b0a11020ff7e0f68
BenKnisley <benknisley@gmail.com>
2020-07-13 14:33:49 -0400

    Refactored RasterLayer - removed unused imports - added code comments - renamed variables

--- PyMapKit/RasterLayer.py
+++ PyMapKit/RasterLayer.py
@@ -1,80 +1,81 @@
 #!/usr/bin/env python3
 """
 Author: Ben Knisley [benknisley@gmail.com]
-Date: Febuary 8, 2020
+Date: February 8, 2020
 """
-import os
-from osgeo import ogr
-import numpy as np
 import gdal
-import pyproj
 import tempfile
 import cairo
 from PIL import Image
-
+ 
 class RasterLayer:
     """ """
-    def __init__(self, path):
+    def __init__(self, path, clear_nodata=True):
         """ """
-        ## Init MapEngine object
+        ## Open path with GDAL, if it fails raise exception
+        self._gdal_raster = gdal.Open(path)
+        if self._gdal_raster == None:
+            raise Exception(f"{path}: Either doesn't exist, or isn't a valid raster file.")
+
+        ## Set variables from args 
+        self.path = path
+
+        #$ clear_nodata flag says whether to make nodata pixels transparent 
+        self.clear_nodata = clear_nodata
+
+        ## Setup placeholder values
         self._MapEngine = None
+        self._image_surface = None
+        self._proj_x = None
+        self._proj_y = None
+        self._scale_x = None
+        self._scale_y = None
 
-        ## open raster with gdal
-        self.gdal_raster = gdal.Open(path)
-
-        ## Extract projection, scale, and location from raster
-        self._init_proj = pyproj.Proj(self.gdal_raster.GetProjection())
-
-        ##
-        self._init_proj_x, self._init_scale_x, _, self._init_proj_y, _, self._init_scale_y = self.gdal_raster.GetGeoTransform()
-
-    
     def _activate(self, new_MapEngine):
         """ Function called when layer is added to a MapEngine layer list."""
+        ## Update MapEngine 
         self._MapEngine = new_MapEngine
 
-        ## Create a tempfile geotif file
-        temp_tif = tempfile.NamedTemporaryFile(suffix='.tif', delete=False)			      
-	      
-        ## Reproject to mapengines projection into temp_tif 
-        gdal.Warp(temp_tif.name, self.gdal_raster, dstSRS=self._MapEngine.get_projection())
+        ## Warp (transform) the original raster to new_MapEngine srs into a tempfile  
+        temp_tif = tempfile.NamedTemporaryFile(suffix='.tif', delete=False)
+        gdal.Warp(temp_tif.name, self._gdal_raster, dstSRS=self._MapEngine.get_projection())
 
+        ## Open the tempfile with GDAL and extract proj coords and scale
+        temp_tiff = gdal.Open(temp_tif.name)
+        self._proj_x, self._scale_x, _, self._proj_y, _, self._scale_y = temp_tiff.GetGeoTransform()
 
-        ## Create a temp PNG 
-        ## Load/convert temp_tif into temp_png
-        temp_png = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-        
         #Image.MAX_IMAGE_PIXELS = 9331200000000
-        im = Image.open(temp_tif.name)
         
-        im = im.convert("RGBA")
-        datas = im.getdata()
+        ## Load image data with PIL
+        image = Image.open(temp_tif.name)
 
-        ## Convert pure black pixels to transpancy 
-        newData = []
-        for item in datas:
-            if item[0] == 0 and item[1] == 0 and item[2] == 0:
-                newData.append((255, 255, 255, 0))
-            else:
-                newData.append(item)
+        ## If clear_nodata flag is set, make nodata pixels transparent
+        if self.clear_nodata:
+            ## Convert to RGBA colorspace, and get image_data
+            image = image.convert("RGBA")
+            image_data = image.getdata()
+            
+            ## Loop through all pixels: loading them into new_image_data
+            new_image_data = []
+            for pixel in image_data:
+                if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+                    new_image_data.append( (255, 255, 255, 0) )
+                else:
+                    new_image_data.append(pixel)
 
-        im.putdata(newData)
+            ## Put manipulated data into image
+            image.putdata(new_image_data)
         
-        im.save(temp_png.name, "PNG")
+        ## Save image data into a tempfile, converting into PNG
+        temp_png = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+        image.save(temp_png.name, "PNG")
         
-
-
-
-        self.image_surface = cairo.ImageSurface.create_from_png(temp_png.name)
-
-        ## 
-        self._proj_x, self._proj_y =  pyproj.transform(self._init_proj, self._MapEngine.get_projection(), self._init_proj_x, self._init_proj_y)
-
+        ## Cache image data
+        self._image_surface = cairo.ImageSurface.create_from_png(temp_png.name)
 
     def _deactivate(self):
         """ Function called when layer is added to a MapEngine """
         pass
-
 
     def draw(self, renderer, cr):
         """ """
@@ -82,13 +83,13 @@ class RasterLayer:
 
         cr.save()
         
-        scale_fact_x = abs( self._init_scale_x / self._MapEngine._scale )
-        scale_fact_y = abs( self._init_scale_y / self._MapEngine._scale )
+        scale_fact_x = abs( self._scale_x / self._MapEngine._scale )
+        scale_fact_y = abs( self._scale_y / self._MapEngine._scale )
         
         cr.translate(pix_x, pix_y)
         cr.scale(scale_fact_x, scale_fact_y)
 
-        cr.set_source_surface(self.image_surface)
+        cr.set_source_surface(self._image_surface)
 
         cr.paint()
         cr.restore()
