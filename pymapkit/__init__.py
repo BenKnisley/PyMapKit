23e2b217b8a0ea7338349ce8bee1681922d63c36
BenKnisley <benknisley@gmail.com>
2021-01-10 20:20:55 -0500

    Added __init__.py & setup.py Files

---
+++ pymapkit/__init__.py
@@ -0,0 +1,19 @@
+"""
+Project: PyMapKit
+Title: PyMapKit
+Function: PyMapKit is a Python based open-source mapping toolkit.
+Author: Ben Knisley [benknisley@gmail.com]
+Created: 1 January, 2020
+"""
+## Define metadata
+__author__ = "Ben Knisley"
+__maintainer__ = "Ben Knisley"
+__email__ = "benknisley@gmail.com"
+__copyright__ = "Copyright 2021, Ben Knisley"
+__license__ = "MIT"
+__version__ = "0.1.5"
+__status__ = "Pre-Alpha"
+
+## Import Map class
+from .map import Map
+
