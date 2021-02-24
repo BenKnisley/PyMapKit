91ebafdb66f7fa533a29fc5a8c49f7cc6a1cceff
BenKnisley <benknisley@gmail.com>
2021-02-24 15:28:55 -0500

    Added @abstractmethod to BaseLayer Class Methods

--- pymapkit/base_layer.py
+++ pymapkit/base_layer.py
@@ -2,16 +2,13 @@
 Author: Ben Knisley [benknisley@gmail.com]
 Date: 19 January, 2020
 """
-
-
+import abc
 
 class BaseLayer:
     def __init__(self):
         ## Required Attributes
         self.name = None ## Holds the name of the layer
-        
         self.map = None ## Holds the parent map when activated
-        
         self.status = False ## Signals what the layer is doing or need done ==>
         ## => Such as 'loading', 'downloading', 'projecting', 'rendering', 'done'
     
@@ -21,6 +18,7 @@ class BaseLayer:
         self.map = new_parent
         self.activate()
 
+    @abc.abstractmethod
     def activate(self):
         ''' Method called by _activate. Holds functions to be preformed to activate layer''' 
         pass
@@ -30,27 +28,31 @@ class BaseLayer:
         self.parent = None
         self.deactivate()
 
+    @abc.abstractmethod
     def deactivate(self):
         ''' Method called by parent Map object when self is removed from it '''
         pass
 
-
+    @abc.abstractmethod
     def __repr__(self):
         ''' What string should represent the layer '''
         pass
     
-
+    @abc.abstractmethod
     def get_extent(self):
         ''' Method that returns bounding box of all stored data'''
         pass
     
+    @abc.abstractmethod
     def focus(self):
         ''' Method to move map to focus on layer '''
         pass
     
+    @abc.abstractmethod
     def set_opacity(self):
         ''' Method to set opacity of whole layer '''
     
+    @abc.abstractmethod
     def draw(self, renderer, canvas):
         ''' Method '''
         pass
