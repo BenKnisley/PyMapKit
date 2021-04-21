346434d478f40fe1b321b7417f9c7fd1d4342859
BenKnisley <benknisley@gmail.com>
2021-04-21 15:12:47 -0400

    Fully Implemented RasterLayer with Doc Strings
    Still needs tests, but otherwise ready

--- pymapkit/raster_layer.py
+++ pymapkit/raster_layer.py
@@ -2,108 +2,208 @@
 Author: Ben Knisley [benknisley@gmail.com]
 Date: 19 April, 2021
 """
+import os
 import tempfile
 import gdal
+import pyproj
 from .base_layer import BaseLayer
 
 
 class RasterLayer(BaseLayer):
+    """
+    A class providing a single raster image as a map layer.
+    """
+
     def __init__(self, path):
-        self.name = "Raster Layer"
+        """
+        RasterLayer Class Constructor
+
+        Initializes a raster layer from a given path.
+
+        Args:
+            path (str): The path of the file containing the raster.
+        
+        Returns:
+            None
+        """
+        ## Update layer status & name
         self.status = 'initializing'
+        self.name = "Raster Layer"
+        
+        ## Open given path with GDAL, if loading fails raise exception
+        self.gdal_raster = gdal.Open(path)
+        if self.gdal_raster == None:
+            raise Exception(f"{path}: file either doesn't exist, or isn't a valid raster file.")
 
-        ## Open path with GDAL, if it fails raise exception
-        self._gdal_raster = gdal.Open(path)
-        if self._gdal_raster == None:
-            raise Exception(f"{path}: Either doesn't exist, or isn't a valid raster file.")
-
-        ## Set variables from args 
+        ## Store path of original raster data, & update name
         self.path = path
-        self.alpha = 1
-
-        ##
+        self.name = os.path.basename(path)
+        
+        ## Set up mutable RGB for raster nodata value
         self.nodata_value = (0,0,0)
 
-        ## Setup placeholder values
-        self._img_path = None
-        self._image_surface = None
-        self._proj_x = None
-        self._proj_y = None
-        self._scale_x = None
-        self._scale_y = None
-        
+        ## Init alpha value
+        self.alpha = 1
 
-    
-    def activate(self):
-        """ 
-        Function called when layer is added to a Map Object.
+        ## Setup location and placement variables
+        self.proj_x = None
+        self.proj_y = None
+        self.scale_x = None
+        self.scale_y = None
+
+        ## Setup image storage variables
+        self.image_path = None
+        self.image_cache = None
+        
+        ## Update Status
+        self.status = 'initialized'
+
+    def __repr__(self):
         """
-        ## Warp (transform) the original raster to parent maps srs into a tempfile  
+        Returns string representation of layer.
+        """
+        return f"Raster Layer: {self.name} ({self.path})"
+
+    def activate(self):
+        """
+        Activates the layer after the layer is added to a map object.
+
+        Reprojects raster image to the parent maps; projection, and copies 
+        image data into a temporary png file with nodata values transparent.
+
+        Args:
+            None
+        
+        Returns:
+            None
+        """
+        ## Update Status
+        self.status = 'loading'
+        
+        ## Create a temp raster file to store reprojected raster
         temp_tif = tempfile.NamedTemporaryFile(suffix='.tif', delete=False)
 
-        ## GDAL Warp _gdal_raster parent projection into temp file
-        '''
-        if self.srcSRS:
-            gdal.Warp(temp_tif.name, self._gdal_raster, srcSRS=self.srcSRS, dstSRS=self.parent.get_projection())
-        else:
-            gdal.Warp(temp_tif.name, self._gdal_raster, dstSRS=self.parent.get_projection())
-        '''
-        gdal.Warp(temp_tif.name, self._gdal_raster, dstSRS=self.map.projected_crs)
+        ## Reproject & load gdal raster into temp raster file
+        gdal.Warp(temp_tif.name, self.gdal_raster, dstSRS=self.map.projected_crs)
 
-        ## Open the tempfile with GDAL and extract proj coords and scale
+        ## Open the temp raster with GDAL and extract proj coords and scale
         temp_tiff = gdal.Open(temp_tif.name)
-        self._proj_x, self._scale_x, _, self._proj_y, _, self._scale_y = temp_tiff.GetGeoTransform()
+        self.proj_x, self.scale_x, _, self.proj_y, _, self.scale_y = temp_tiff.GetGeoTransform()
 
-        ## Get Each color band
+        ## Set nodata values for each band, so it is transparent in png file
         R_band = temp_tiff.GetRasterBand(1)
         G_band = temp_tiff.GetRasterBand(2)
         B_band = temp_tiff.GetRasterBand(3)
-
-        ## Set given nodata values
         R_band.SetNoDataValue(self.nodata_value[0])
         G_band.SetNoDataValue(self.nodata_value[1])
         B_band.SetNoDataValue(self.nodata_value[2])
 
-        ## Save image data into temp png file
+        ## Use GDAL to save image data into temp png file
+        png_driver = gdal.GetDriverByName('PNG')
         temp_png = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-        dst_driver = gdal.GetDriverByName('PNG')
-        dst_driver.CreateCopy(temp_png.name, temp_tiff, 0, ["ZLEVEL=1"])
+        png_driver.CreateCopy(temp_png.name, temp_tiff, 0, ["ZLEVEL=1"])
 
-        ## Keep path of image data to cache during draw 
-        self._img_path = temp_png.name
-    
+        ## Save temp_png path as image_path
+        self.image_path = temp_png.name
+
+        ## Update layer status
+        self.status = 'ready'
+
     def deactivate(self):
-        ''' Method called by parent Map object when self is removed from it '''
-        self._image_surface = None
-        self._proj_x = None
-        self._proj_y = None
-        self._scale_x = None
-        self._scale_y = None
+        """
+        Deactivates layer, making layer available to add to a new map object.
 
-
-    def __repr__(self):
-        ''' What string should represent the layer '''
-        return f"Raster Image from {self.path}"
-    
-    def get_extent(self):
-        ''' Method that returns bounding box of all stored data'''
-        pass
-    
-    def focus(self):
-        ''' Method to move map to focus on layer '''
-        pass
-    
-    def set_opacity(self):
-        ''' Method to set opacity of whole layer '''
-    
-    def render(self, renderer, canvas):
-        ''' Method '''
-        if self._image_surface == None:
-            self._image_surface = renderer.cache_image(self._img_path)
-
-        pix_x, pix_y = self.map.proj2pix(self._proj_x, self._proj_y)
-
-        scale_x = abs( self._scale_x / self.map._proj_scale )
-        scale_y = abs( self._scale_y / self.map._proj_scale )
+        Args:
+            None
         
-        renderer.draw_image(canvas, self._image_surface, pix_x, pix_y, scale_x, scale_y)
+        Returns:
+            None
+        """
+        self.status = 'deactivating'
+        self.image_cache = None
+        self.proj_x = None
+        self.proj_y = None
+        self.scale_x = None
+        self.scale_y = None
+        self.status = 'initialized'
+
+    def get_extent(self):
+        """
+        Returns the extent of the raster data, using the map projections 
+        coordinates system.
+
+        Args:
+            None
+
+        Returns:
+            geo_x_min (float): The min x value of the data
+            geo_y_min (float): The min y value of the data
+            geo_x_max (float): The max x value of the data
+            geo_y_max (float): The max y value of the data
+        """
+        ##
+        raster_x_min, x_fact, _, raster_y_min, _, y_fact = self.gdal_raster.GetGeoTransform()
+
+        ##
+        raster_x_max = raster_x_min + (self.gdal_raster.RasterXSize * x_fact)
+        raster_y_max = raster_y_min + (self.gdal_raster.RasterYSize * y_fact)
+
+        ##
+        raster_crs = pyproj.crs.CRS(self.gdal_raster.GetProjection())
+
+        ##
+        proj_x_vals, proj_y_vals = pyproj.transform(
+            raster_crs, self.map.projected_crs, 
+            (raster_x_min, raster_x_max), (raster_y_min, raster_y_max)
+        )
+        
+        ## Unpack 
+        proj_x_min, proj_x_max = proj_x_vals
+        proj_y_min, proj_y_max = proj_y_vals
+
+        return proj_x_min, proj_y_min, proj_x_max, proj_y_max
+
+    def set_opacity(self, opacity):
+        """
+        Set the opacity of the whole layer.
+        
+        Args:
+            opacity (float): New opacity value 0 to 1.
+
+        Returns:
+            None
+        """
+        self.alpha = opacity
+
+    def render(self, renderer, canvas):
+        """
+        Renders the data onto the given canvas, using the given renderer.
+
+        Args:
+            renderer (BaseRenderer): The renderer object to use the render the
+            data.
+
+            canvas (BaseCanvas): The canvas object to render the data onto.
+        
+        Returns:
+            None
+        """
+        ## Update Status
+        self.status = 'rendering'
+
+        ## Cache image if required
+        if self.image_cache == None:
+            self.image_cache = renderer.cache_image(self.image_path)
+
+        ## Get pixel location
+        pix_x, pix_y = self.map.proj2pix(self.proj_x, self.proj_y)
+
+        ## Calculate scale 
+        scale_x = abs( self.scale_x / self.map._proj_scale )
+        scale_y = abs( self.scale_y / self.map._proj_scale )
+        
+        ## Draw image onto canvas
+        renderer.draw_image(canvas, self.image_cache, pix_x, pix_y, scale_x, scale_y, opacity=self.alpha)
+
+        ## Update Status
+        self.status = 'ready'
