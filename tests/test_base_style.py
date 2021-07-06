45975cfe1dade05e9c62143c05e35f2915613ddc
BenKnisley <ben@benknisley.com>
2021-07-05 20:17:45 -0400

    Adds Some Test Cases for BaseStyle.

---
+++ tests/test_base_style.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""
+Author: Ben Knisley [benknisley@gmail.com]
+Date: 29 June, 2020
+"""
+import pytest
+from unittest.mock import Base, MagicMock
+import pymapkit as pmk
+
+
+class MockMap:
+    def __init__(self):
+        ## Create a mock draw function
+        self.set_projection_coordinates = MagicMock()
+        self.set_scale = MagicMock()
+
+
+class MockFeature:
+    def __init__(self):
+        self.style = None
+
+
+
+def test_base_style_init():
+    """ Test BaseStyle.__init__ Method """
+    f = MockFeature()
+    s = pmk.BaseStyle(f)
+    assert isinstance(s, pmk.BaseStyle)
+
+def test_create_domain_mode_etters():
+    """ Test BaseStyle.create_domain_mode_etters Method """
+    ## Creates a MockFeature and a BaseStyle Object
+    f = MockFeature()
+    s = pmk.BaseStyle(f)
+
+    ## Call method without args
+    s.create_domain_mode_etters()
+    
+    ## Assert that method binds methods correctly
+    ## Does not test bound methods
+    assert "set_display" in f.__dict__
+    assert "get_display" in f.__dict__
+    assert "get_display" in s.__dict__
+    
+    ## Call method with domain set domain_name
+    s.create_domain_mode_etters(domain_name='test')
+    
+    ## Assert that method binds methods correctly
+    ## Does not test bound methods
+    assert "set_test_display" in f.__dict__
+    assert "get_test_display" in f.__dict__
+    assert "get_test_display" in s.__dict__
+
+def test_create_domain_mode_etters_inner_fns():
+    assert True
+
+
+def test_create_property_etters():
+    """ Test BaseStyle.create_property_etters Method """
+    ## Creates a MockFeature and a BaseStyle Object
+    f = MockFeature()
+    s = pmk.BaseStyle(f)
+
+    s.create_property_etters('bingo')
+
+    assert "set_bingo" in f.__dict__
+    assert "get_bingo" in f.__dict__
+    assert "get_bingo" in s.__dict__
+
+def test_create_property_etters_inner_fns():
+    pass
+
+def test_clear_cache():
+    """ Test BaseStyle.clear_cache Method """
+    ## Create a parent object, and a BaseStyle object
+    f = MockFeature()
+    s = pmk.BaseStyle(f)
+
+    ## Set a cached renderer
+    s.cached_renderer_fn = object()
+
+    ## Call method
+    s.clear_cache()
+
+    ## Test that the renderer was cleared
+    assert s.cached_renderer_fn == None
+
+
+    
